4b694536be42972b187c08a679d7d22c3ecfd4d9
zanaalma <zanaalma1@gmail.com>
2019-11-20 23:04:35 +0300

    home work number 4

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Create a file in this repository named `homework.js` to save your solutions.
   <li>Create a function that will get the sum of the numbers between 1 and n and return the answer
   <br>
     <code>summation(5) // should return 15 because 1+2+3+4+5=15</code>
+  
+
   </li>
   <li>Create a program to get the sum of all the even numbers in a group
   <br>
---
+++ hwNum4.js
@@ -0,0 +1,72 @@
+q:11111111
+
+// function summation(n){
+//     var sum = 0
+    
+//     for (let i= 1; i <= n ; i ++){
+       
+//      sum = sum+i
+     
+    
+    
+//     console.log(sum)
+// } 
+
+// summation(5)
+
+q:2222222222
+
+// function summtionEven (para){
+//     var sum = 0;
+//     for(var i = 1; i<= 6; i++) {
+//     if(i % 2 === 0) {
+//     sum = sum + i;
+//     }
+//     }
+//     // return sum;
+//     console.log( sum);
+//     }
+//     summtionEven (5)
+
+
+q:3333333333333
+
+// var numbers = [1 , 2 ,3 ,4] 
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   sum= sum / numbers.length
+  
+//   Console.log(number)
+// }
+
+q:4444444444444
+
+// let array = ["w" ,"o" ,"r" ,"d"]
+// array.reverse()
+
+// console.log(array)
+
+q:55555555555
+// let test0 = ["test1","test2","test3"];
+
+// console.log(test0.join(" - "))
+
+
+
+q:6666666666
+// let countupanddown =[1,2,3,4]
+// countupanddown.reverse()
+// const countdown = function(x,y,z){
+
+// console.log(countupanddown(countdown(1,2,3))
+
+// } 
+
+q:77777777777
+
+// let animals = ["cat" , "rabbit", " dog"];
+// let filterItems= (letters) => {
+//     return animals.filter(animal => animal.indexOf(letters) > -1); 
+// }
+// console.log(filterItems("a")) ;
+
