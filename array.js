f6c2be81e8cc09249e96ef20273a5a2c2b4684ae
roush55 <ranaabumelha@gmail.com>
2019-11-21 04:49:54 +0300

    homework

---
+++ array.js
@@ -0,0 +1,9 @@
+num=[20]
+const x=function(){
+for(i in num);
+    i=1
+    z=i+x[i]
+    console.log(z)
+    return z
+    
+}
---
+++ arraysolutions.js
@@ -0,0 +1,28 @@
+
+num=[20]
+const x=function(){
+for(i in num);
+    i=1
+    z=i+x[i]
+    console.log(z)
+    return z
+    
+}
+ let nums=[1,2,3,4,5]
+function sumArray(array) {
+    for (
+        var
+        index = 0,              // The iterator
+        length = array.length,  // Cache the array length
+        sum = 0;                // The total amount
+        index < length;         // The "for"-loop condition
+        sum += array[index++]   // Add number on each iteration
+    )
+    return sum;
+  }
+ console.log(sumArray(nums))
+ const name =['caterpillar'];
+ console.log(name.reverse(index))
+
+
+
