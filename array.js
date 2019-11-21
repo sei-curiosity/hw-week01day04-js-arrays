9046aba373f2f8ed5777d32fff4af707b52e9885
roush55 <ranaabumelha@gmail.com>
2019-11-21 06:47:12 +0300

    function works

--- array.js
+++
@@ -1,9 +0,0 @@
-num=[20]
-const x=function(){
-for(i in num);
-    i=1
-    z=i+x[i]
-    console.log(z)
-    return z
-    
-}
--- arraysolutions.js
+++ arraysolutions.js
@@ -1,28 +1,33 @@
 
-num=[20]
-const x=function(){
-for(i in num);
-    i=1
-    z=i+x[i]
-    console.log(z)
-    return z
+// num=[20]
+// const x=function(){
+// for(i in num);
+//     i=1
+//     z=i+x[i]
+//     console.log(z)
+//     return z
     
-}
- let nums=[1,2,3,4,5]
-function sumArray(array) {
-    for (
-        var
-        index = 0,              // The iterator
-        length = array.length,  // Cache the array length
-        sum = 0;                // The total amount
-        index < length;         // The "for"-loop condition
-        sum += array[index++]   // Add number on each iteration
-    )
-    return sum;
-  }
- console.log(sumArray(nums))
- const name =['caterpillar'];
- console.log(name.reverse(index))
+var numM=[];
+    numM=[1,2,4,5];
+    function sumArray(array) {
+        for (
+          var
+            index = 0,              // The iterator
+            length = array.length,  // Cache the array length
+            sum = 0;                // The total amount
+            index < length;         // The "for"-loop condition
+            sum += array[index++]   // Add number on each iteration
+        );
+        return sum;
+      }
+ console.log(sumArray(numM))
+  let name=[];
+  name=['caterpillar'];
+        name[index].reverse();
 
 
 
+       name.reverse();
+  //console.log()
+
+ //console.log(name.index.reverse())
